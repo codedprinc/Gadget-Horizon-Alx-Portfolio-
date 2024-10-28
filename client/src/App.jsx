7907a1d44fa48718ef16d972bb9d8964976bef68
“Matt <mattkeneth@gmail.com>
2024-10-28 08:41:00 +0300

    Make react app Made mandatory folders or directories
    Copied package.json
    
    Ran npm to install packages
    
    Tested main react app or component
    
    Changed html icon to gagdget logo
    
    Added README.md file

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Routes, Route } from 'react-router-dom'
+
+
+const App = () => {
+  return (
+      <div className='bg-red-400 text-white'>
+        <h1 className='text-white'>Hello</h1>
+      </div>
+  )
+}
+export default App;
